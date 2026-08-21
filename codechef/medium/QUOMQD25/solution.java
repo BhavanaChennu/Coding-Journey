71bdf53665ccd86ca1c3b4c6fd4bdd7ef3f35e5b
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-21 16:43:35 +0530

    ✅ QUOMQD25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/QUOMQD25/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        String sentence = "apple pie";
+
+        // Find the indices of 'p' and 'e'
+        int indexOfP = sentence.indexOf('p');
+        int indexOfE = sentence.indexOf('e');
+
+        // Compare indices using relational operators
+        boolean isPBeforeE = (indexOfP < indexOfE);
+
+        // Print the comparison result
+        System.out.println("Is 'p' before 'e'? "+isPBeforeE);
+
+    }
+}
