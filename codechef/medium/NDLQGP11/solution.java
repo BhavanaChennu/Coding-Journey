1f3aad7d3126a65a326b19ee65a1e8456970091e
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-21 17:16:20 +0530

    ✅ NDLQGP11 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/NDLQGP11/solution.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        // Define a variable productName and its value 
+        String productName = "Samsung GALAXY Phone";
+        
+        // Use toLowerCase() method to print the product name in lowercase for storage
+        String formatted = productName.toLowerCase();
+        
+        // Print the desired results
+        System.out.println("Formatted Product Name: "+ formatted);
+        
+    }
+}
