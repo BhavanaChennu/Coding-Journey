338cd6db56499fe8d4d5433d97e2a01e02e76a4a
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-21 16:45:35 +0530

    ✅ QUOMQD30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/QUOMQD30/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Define a string
+        String sentence = "Learning Java is fun. Java is powerful.";
+
+        // Find the first occurrence of the word "Java" using indexOf()
+        int firstIndexOfJava = sentence.indexOf("Java");// Your code here
+
+        // Print the result
+        System.out.println("The first occurrence of 'Java' is at index: " + firstIndexOfJava);
+    }
+}
