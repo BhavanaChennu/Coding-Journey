189457cb4c513281667365d0b906b9619fad74c9
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-21 17:30:58 +0530

    ✅ NDLQGP25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/NDLQGP25/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        String description = "This shirt is available in multiple colours.";
+        String modified = description.toUpperCase().replace("colour" , "color");
+        System.out.println(modified);
+    }
+}
