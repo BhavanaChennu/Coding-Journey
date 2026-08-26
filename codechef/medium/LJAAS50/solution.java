8f81398a295392c9f5c97cb1f98e33bad15c903e
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-26 22:58:19 +0530

    ✅ LJAAS50 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/LJAAS50/solution.java
+++ codechef/medium/LJAAS50/solution.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
+
 class Codechef
 {
 	public static void main (String[] args) 
 	{
 		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int radius = sc.nextInt();
-		double area = 3.14 * radius * radius;
-		System.out.println(area);
-        sc.close();
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.next();
+        String input2 = sc.next();
+        System.out.println(input1 + " " + input2);
 	}
 }
