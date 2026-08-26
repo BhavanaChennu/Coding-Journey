fa1e67719cb0f9baaa74c5d0a09b40e372454788
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-26 23:22:05 +0530

    ✅ LJAAS59 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/LJAAS59/solution.java
+++ codechef/medium/LJAAS59/solution.java
@@ -6,22 +6,31 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         int num;
-        num = scanner.nextInt(); // input first number
-        // Check first number
-        if( num > 0)
+        num = scanner.nextInt();
+
+        if (num > 0)
             System.out.println("Positive");
-        
-        num = scanner.nextInt(); // input second number
-        // Check second number
-        if(num == 0){
-            System.out.println("Zero");
-        }
-        
-        num = scanner.nextInt(); // input third number
-        // Check third number
-        if(num < 0){
+        else if (num < 0)
             System.out.println("Negative");
-        }
-        
+        else
+            System.out.println("Zero");
+
+        num = scanner.nextInt();
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+
+        num = scanner.nextInt();
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
     }
 }
