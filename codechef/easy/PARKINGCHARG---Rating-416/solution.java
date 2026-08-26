5e39ddbebfac959a43d89a7dd4f874e6919ec55d
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-26 23:13:14 +0530

    ✅ PARKINGCHARG - Rating 416 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/easy/PARKINGCHARG---Rating-416/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int h = sc.nextInt();
+		int res = x + y * (h -1);
+		System.out.println(res);
+
+	}
+}
