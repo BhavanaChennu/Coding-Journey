16709b781ffc9bb51a9027b7f67b00106dcfb587
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-26 22:53:40 +0530

    ✅ LJAAS48 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/LJAAS48/solution.java
+++ codechef/medium/LJAAS48/solution.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int one = scanner.nextInt();
-        int two = scanner.nextInt();
-        System.out.println(one / two);
+        String name = scanner.next();
+        System.out.println(name.charAt(7));
     }
 }
+
