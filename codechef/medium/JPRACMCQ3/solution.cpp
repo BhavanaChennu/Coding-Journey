a73eb02dd87275f77973636b0303d6175893d902
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-26 23:11:24 +0530

    ✅ JPRACMCQ3 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-26

---
+++ codechef/medium/JPRACMCQ3/solution.cpp
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int newrows = sc.nextInt();
+        
+        int totalCells = (rows + newrows) * cols;
+        System.out.println(totalCells);
+	}
+}
