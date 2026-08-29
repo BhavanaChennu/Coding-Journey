812a86a9a30934c3988eddad3cd5729bbab605ba
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-29 15:44:34 +0530

    ✅ CTMNMF19 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

--- codechef/medium/CTMNMF19/solution.java
+++ codechef/medium/CTMNMF19/solution.java
@@ -1,24 +1,21 @@
+import java.util.Scanner;
 class Codechef {
     public static void main(String[] args) {
-        // Define the maximum number limit for the calculation
-        int maxNumber = 20; 
+        
+        Scanner sc = new Scanner(System.in);
+        //Take input of n from user
+        int n= sc.nextInt();
+        
+        int number = 2; // Starting with the first even number
+        
+        System.out.println("Even numbers from 2 to " + n + ":");
+        
+        while (number <= n) {  // Continue loop until number is less than or equal to n
+            System.out.println(number);
+            number += 2;
 
-        // Initialize sum to store the total sum of numbers
-        int sum = 0; 
-
-        // Start from 1 as the first number in the sequence
-        int currentNumber = 1;  
-
-        // While loop to iterate as long as currentNumber is less than or equal to maxNumber
-        while (currentNumber <= maxNumber) {
-            // Add the current number to the sum
-            sum += currentNumber;  
-
-            // Move to the next third number by incrementing by 3
-            currentNumber += 3;    
         }
-
-        // Print the final sum of every third number up to the given maxNumber
-        System.out.println("The sum of every third number up to " + maxNumber + " is: " + sum);
+        sc.close();
     }
 }
+
