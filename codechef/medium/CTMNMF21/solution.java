72c222895378636fac2747cecdbe7d1506de97a7
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-29 15:47:01 +0530

    ✅ CTMNMF21 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

---
+++ codechef/medium/CTMNMF21/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Codechef {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        //Take input of n from user
+        int n= sc.nextInt();
+        
+        int number = 2; // Starting with the first even number
+        
+        System.out.println("Even numbers from 2 to " + n + ":");
+        
+        while (number <= n) {  // Continue loop until number is less than or equal to n
+            System.out.println(number);
+            number += 2;
+
+        }
+        sc.close();
+    }
+}
+
