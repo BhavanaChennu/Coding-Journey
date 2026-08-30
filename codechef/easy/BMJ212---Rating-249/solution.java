b7942362a71931dcd5227ee6abcbda6ac0ed15ef
Bhavana Chennu <bhavanachennu61@gmail.com>
2026-08-30 22:26:57 +0530

    ✅ BMJ212 - Rating 249 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-30

---
+++ codechef/easy/BMJ212---Rating-249/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		for(int i=0; i<t; i++)
+		//Update your code below this line to solve the problem
+		{
+    	    int a = read.nextInt();
+    	    
+    	    int asquats = a*15;
+            System.out.println(asquats);
+    	
+		}
+	}
+}
